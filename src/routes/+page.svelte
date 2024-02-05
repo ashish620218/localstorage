364d0aab3e609b57ae905995f43263680f7072de
ashish <ashish620218@gmail.com>
2024-02-05 17:30:58 +0530

    add table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,39 @@
-<script>
-  let form = {
+<script lang="ts">
+  import {onMount} from 'svelte'
+  let form = {    
     name: "",
     country: "",
     email: "",
     password: "",
   };
-  function submitForm() {
-    const nameInput = form.name.trim();
 
-    if (nameInput === "") {
+  let localStorageData:any[]=[];
+
+  function f(){
+   for (let i=0;i<localStorage.length;i++){
+    const key=localStorage.key(i);
+    if(key){
+      localStorageData.push(JSON.parse(localStorage.getItem(key)??''))
+    }
+   }
+   localStorageData=localStorageData   
+  }
+  
+  function submitForm() { 
+    const nameInput = form.name.trim();
+    
+    if (nameInput === "") {  
       alert("Enter the name");
       return;
     }
-
+    
     const countryInput = form.name.trim();
-
+ 
     if (countryInput === "") {
       alert("Enter the country name");
       return;
     }
-
+    
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(form.email)) {
       alert("Email is not valid");
@@ -36,6 +50,14 @@
     
     localStorage.setItem(form.email , JSON.stringify({ Name: form.name, Email: form.email, Country: form.country }));
   }
+
+  onMount(()=>{
+    f();
+    console.log(localStorageData);
+  })
+
+  
+    
 </script>
 
 <main class="min-h-screen flex items-center justify-center bg-[#f1e7f8]">
@@ -94,5 +116,28 @@
     >
       Submit
     </button>
+
   </div>
 </main>
+  <div class="flex justify-center bg-[#f1e7f8]">
+    <table class="bg-[#e9e1f8] border-gray-300 w-[70] mb-20">
+        <thead>
+            <tr>
+                <th class="py-2 px-4 border-b">Name</th>
+                <th class="py-2 px-4 border-b">Country</th>
+                <th class="py-2 px-4 border-b">Email</th>
+            </tr>
+        </thead>
+            <tbody>
+              {#if localStorageData.length>0}
+              {#each localStorageData as data}
+              <tr>
+                <td class="py-2 px-4 border-b">{data.Name}</td>
+                <td class="py-2 px-4 border-b">{data.Country}</td>
+                <td class="py-2 px-4 border-b">{data.Email}</td>
+              </tr>
+              {/each}
+              {/if}
+            </tbody>
+    </table>
+  </div>
